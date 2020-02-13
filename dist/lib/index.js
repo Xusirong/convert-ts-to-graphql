38b28b0f05450a118e84fbe541631958ca329d79
sr.xu <sr.xu@ctrip.com>
2020-02-13 22:22:54 +0800

    build

--- dist/lib/index.js
+++ dist/lib/index.js
@@ -4,49 +4,81 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
 const convert_1 = __importDefault(require("./convert"));
 const defaultOptions = {
     baseUrl: __dirname,
     anyType: "Any"
 };
-/**
- *
- * @param filePath .d.ts文件所在位置
- * @param options 配置transfer的行为
- */
-function default_1(options) {
+function convertFile(options) {
     if (!options.inputFile) {
         console.error('参数必须传入inputFile');
         return false;
     }
-    if (!options.outFile) {
-        console.error('参数必须传入outFile');
-        return false;
-    }
-    let inputFile = normPath(options.inputFile);
-    let outFile = normPath(options.outFile);
     let baseUrl = options.baseUrl || defaultOptions.baseUrl;
     let anyType = options.anyType || defaultOptions.anyType;
-    let outDir = '';
-    if (options.outDir) {
-        outDir = normPath(options.outDir);
-        createDir(baseUrl + outDir);
+    let filePath = path_1.default.join(baseUrl, options.inputFile);
+    if (!fs_1.default.existsSync(filePath)) {
+        throw new Error(`${filePath}文件不存在`);
     }
-    let inputPath = baseUrl + inputFile;
-    let outputPath = baseUrl + outDir + outFile;
-    writeFile(outputPath, convert_1.default(inputPath, anyType));
+    return convert_1.default(filePath, anyType);
 }
-exports.default = default_1;
-function normPath(path) {
-    let result = path.split('/').filter(value => {
-        return value !== "";
-    }).join('/');
-    if (result.length > 0) {
-        return '/' + result;
+exports.convertFile = convertFile;
+/**
+ * @param filePath .d.ts文件所在位置
+ * @param options 配置transfer的行为
+ */
+function convertDir(options) {
+    if (!options.inputDir) {
+        console.error('参数必须传入inputDir');
+        return false;
     }
-    else {
-        return '';
+    if (!options.outDir) {
+        console.error('参数必须传入outDir');
+        return false;
     }
+    let baseUrl = options.baseUrl || defaultOptions.baseUrl;
+    let anyType = options.anyType || defaultOptions.anyType;
+    let inputPath = path_1.default.join(baseUrl, options.inputDir);
+    let outputPath = path_1.default.join(baseUrl, options.outDir);
+    // 创建输出的文件夹
+    createDir(outputPath);
+    transferFiles(inputPath, outputPath, anyType);
+}
+exports.convertDir = convertDir;
+function createDir(outDir) {
+    if (!fs_1.default.existsSync(outDir)) {
+        fs_1.default.mkdirSync(outDir);
+    }
+}
+function transferFiles(inputPath, outputPath, anyType) {
+    fs_1.default.readdir(inputPath, function (error, files) {
+        if (error) {
+            console.error(error);
+        }
+        else {
+            files.forEach((filename) => {
+                let input = path_1.default.join(inputPath, filename);
+                let output = path_1.default.join(outputPath, filename);
+                fs_1.default.stat(input, function (err, stats) {
+                    if (err) {
+                        console.error(err);
+                    }
+                    else {
+                        let isFile = stats.isFile(); //是文件
+                        if (isFile) {
+                            writeFile(output, convert_1.default(input, anyType));
+                        }
+                        let isDir = stats.isDirectory(); //是文件夹
+                        if (isDir) {
+                            createDir(output);
+                            transferFiles(input, output, anyType); //递归
+                        }
+                    }
+                });
+            });
+        }
+    });
 }
 function writeFile(outputPath, data) {
     fs_1.default.writeFile(outputPath, data, function (error) {
@@ -56,10 +88,5 @@ function writeFile(outputPath, data) {
         }
         console.log('转化成功');
     });
-}
-function createDir(outDir) {
-    if (!fs_1.default.existsSync(outDir)) {
-        fs_1.default.mkdirSync(outDir);
-    }
 }
 //# sourceMappingURL=index.js.map
--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -1,14 +1,18 @@
-interface TransferOptions {
+interface ConvertFileOptions {
     baseUrl?: string;
     inputFile: string;
-    outDir?: string;
-    outFile: string;
+    anyType?: string;
+}
+declare function convertFile(options: ConvertFileOptions): string | false;
+interface ConvertDirOptions {
+    baseUrl?: string;
+    inputDir: string;
+    outDir: string;
     anyType?: string;
 }
 /**
- *
  * @param filePath .d.ts文件所在位置
  * @param options 配置transfer的行为
  */
-export default function (options: TransferOptions): false | undefined;
-export {};
+declare function convertDir(options: ConvertDirOptions): false | undefined;
+export { convertFile, convertDir };
